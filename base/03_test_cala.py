8bfe73deb90c46ca79459ef345c28414a7157276
baitianming <dawn_bai@126.com>
2022-03-02 15:45:54 +0800

    pytest.ini兼容yaml路径及mark标记

--- base/03_test_cala.py
+++ base/03_test_cala.py
@@ -24,10 +24,11 @@ import yaml
 class Test_cala(object):
     # 全局变量 指定测试数据
     # 加减法共用一套测试数据 加法成功的数据case 减法则失败
-    add_subtraction_yaml = yaml.load(open('../data/add_subtraction_cala.yml'), Loader=yaml.Loader)
+    # 兼容pytest.ini 将yml文件路径改为绝对路径
+    add_subtraction_yaml = yaml.load(open('/Users/zcsy/Documents/Pytest_exercise/data/add_subtraction_cala.yml'), Loader=yaml.Loader)
 
     # 乘除法共用一套测试数据 乘法成功的数据case 除法则失败
-    division_ride_yaml = yaml.load(open('../data/division_ride_cala.yml'), Loader=yaml.Loader)
+    division_ride_yaml = yaml.load(open('/Users/zcsy/Documents/Pytest_exercise/data/division_ride_cala.yml'), Loader=yaml.Loader)
 
     # 初始化
     @pytest.fixture(autouse=True)
@@ -47,15 +48,17 @@ class Test_cala(object):
         assert result == c
 
     @pytest.mark.parametrize('a, b, c', division_ride_yaml)
+    @pytest.mark.execute
     def test_ride(self, a, b, c):
         result = self.cala.ride(a, b)
         assert result == c
 
     @pytest.mark.parametrize('a, b, c', division_ride_yaml)
+    @pytest.mark.execute
     def test_division(self, a, b, c):
         result = self.cala.division(a, b)
         assert result == c
 
 
-if __name__ == '__main__':
-    pytest.main(['-vs'])
+# if __name__ == '__main__':
+    # pytest.main(['-vs'])
