e5dc8ca3d1211da8ff71a46b093b094cd99f90d3
baitianming <dawn_bai@126.com>
2022-03-02 16:28:18 +0800

    yaml基础

--- base/05_yaml引用.py
+++ base/05_yaml引用.py
@@ -2,6 +2,9 @@ import yaml
 with open('../data/test_yaml.yml') as f:
     print(yaml.safe_load(f))
 
+# 输出 [[1, 2, 3], 6, 4, 5]
+with open('../data/test_yaml_two.yml')as f:
+    print(yaml.safe_load(f))
 
 """
     输出
